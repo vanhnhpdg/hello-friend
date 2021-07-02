6f535275d83272bc2a20bb743933ee467b6c5bcf
vanhnhpdg <86811054+vanhnhpdg@users.noreply.github.com>
2021-07-02 10:10:52 +0700

    Update and rename README.md to config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ const  CONFIG = {
      btnIntro: 'hihi',
      title: 'Phải chăng ngồi với tớ cậu có thấy zui <333',
      desc: 'Khoảng thời gian ngồi với tớ cậu có thấy zui hông :)))',
-     btnYes: ' Có, vui chứ =)))',
+     btnYes: 'Có, vui chứ =)))',
      btnNo: 'Không :<<',
      question: 'Tại sao cậu lại thấy vui',
      btnReply: 'Gửi cho Vanh',
      reply: 'Thích thì vui không thích thì vui',
      mess: 'Tớ biết mà <33',
      messLink: 'https://m.me/emm.van.583'
-}       
+}      
